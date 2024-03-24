48b9a0915fa64f437a8ff8a9c540741c60eb6172
Sakin790 <sakinislam79@gmail.com>
2024-03-24 10:56:12 +0600

    mongoose model

--- my-app/src/models/userModel.js
+++ my-app/src/models/userModel.js
@@ -1,1 +1,30 @@
+import { data } from "autoprefixer";
 import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    require: [true, "Please enter username"],
+  },
+  email: {
+    type: String,
+    require: [true, "Please enter email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: [true, "Please enter password"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  forgotPasswordToken: String,
+  forgotPasswordExpiry: Date,
+  verifyToken: String,
+  verifyTokenExpiry: Date,
+});
+
+const User = mongoose.models.users || mongoose.model("users", userSchema);
+
+export default User;
